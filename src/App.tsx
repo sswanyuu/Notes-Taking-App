fabb16b82c8f566779b1f0520460df598cd6c827
Olgaa <yosoywanyushih@gmail.com>
2023-04-12 16:50:40 +0800

    feat: update data when creating note

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { NewNote } from "./NewNote";
-
+import { useLocalStorage } from "./useLocalStorage";
+import { useMemo } from "react";
+import { v4 as uuidV4 } from "uuid";
 export type Note = {
   id: string;
 } & NoteData;
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+export type RawNoteData = {
+  title: string;
+  markDown: string;
+  tagIds: string[];
+};
 export type NoteData = {
   title: string;
   markDown: string;
@@ -17,6 +27,24 @@ export type Tag = {
 };
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes((prevNotes) => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) },
+      ];
+    });
+  }
   return (
     <Container>
       <Routes>
--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
   const [value, setValue] = useState<T>(() => {
     const jsonValue = localStorage.getItem(key);
+    //check if the value is exist in the local storage
     if (jsonValue == null) {
       if (typeof initialValue === "function") {
         return (initialValue as () => T)();
@@ -12,6 +13,7 @@ export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
       return JSON.parse(jsonValue);
     }
   });
+  //update the local storage when the value is changed
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
   }, [key, value]);
